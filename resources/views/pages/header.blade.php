f079c5c919cf54917e9adbc32ac79f1ebccfcc0c
0x776172 <ipul.anwar313@gmail.com>
2023-08-02 16:32:50 +0700

    update2

--- resources/views/pages/header.blade.php
+++ resources/views/pages/header.blade.php
@@ -1,13 +1,17 @@
+@php($active = 'bg-blue-500 text-white')
+@php($inactive = 'hover:border-blue-500 text-blue-400')
+@php($base = 'inline-block py-1 px-3 rounded border border-transparent font-semibold')
+
 <nav class="flex justify-center border-b border-gray-600">
   <ul class="flex m-7">
-    <li class="mr-3">
-      <a class="inline-block py-1 px-3 rounded border border-transparent {{ Request::is('/') ? 'bg-blue-500 text-white' : 'hover:border-blue-500 text-blue-500' }}" href="/">Home</a>
+    <li class="mr-4">
+      <a class="{{ $base }} {{ Request::is('/') ? $active : $inactive }}" href="/">Home</a>
     </li>
-    <li class="mr-3">
-      <a class="inline-block py-1 px-3 rounded border border-transparent {{ Request::is('about') ? 'bg-blue-500 text-white' : 'hover:border-blue-500 text-blue-500' }}" href="/about">About Us</a>
+    <li class="mr-4">
+      <a class="{{ $base }} {{ Request::is('about') ? $active : $inactive }}" href="/about">About Us</a>
     </li>
-    <li class="mr-3">
-      <a class="inline-block py-1 px-3 text-gray-400 cursor-not-allowed" href="#">Disabled Pill</a>
+    <li class="mr-4">
+      <a class="{{ $base }} {{ Request::is('project') ? $active : $inactive }}" href="/project">Projects</a>
     </li>
   </ul>
 </nav>
---
+++ resources/views/pages/project.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+<div>
+  Projects
+</div>    
+@endsection
+
+@section('title', 'Projects | Practice')
