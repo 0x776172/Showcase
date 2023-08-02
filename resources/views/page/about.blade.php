6e3139509399e2b13be63b1b8499083b753e5c5d
0x776172 <ipul.anwar313@gmail.com>
2023-08-02 11:46:40 +0700

    Installing tailwind

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
     <title>About | Practice</title>
   </head>
   <body>
+    <header>
+      @include('page.navbar')
+    </header>
     <h1>Halaman About</h1>
   </body>
 </html>
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -1,21 +1,24 @@
-@include('page/navbar')
-
 <!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/home.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css">
     <title>Home | Practice</title>
   </head>
   <body>
-    <div class="container">
-      <h1 class="mt-4">Halaman Home</h1>
-      <p>{{ $name }}</p>
+    <div>
+      <header>
+        @include('page.navbar')
+      </header>
+      <div class="h-screen flex align-bottom">
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <p class="underline">{{ $name }}</p>
+      </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
   
 </html>
